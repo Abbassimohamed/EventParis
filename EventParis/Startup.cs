2710b192549a954af3eb4eba18520ecdd2493126
anassbb <anasabassi11@gmail.com>
2019-01-03 12:34:05 +0100

    Add project files.

---
+++ EventParis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventParis.Startup))]
+namespace EventParis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
